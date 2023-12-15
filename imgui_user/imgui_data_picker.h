e9f7a9e3883d198361fa3208e66f7b429b0eea82
YUZHIGUIYI <13601462025@163.com>
2023-12-15 20:47:36 +0800

    Fix compilation error when referencing static library

--- imgui_user/imgui_data_picker.h
+++ imgui_user/imgui_data_picker.h
@@ -14,17 +14,17 @@
 namespace ImGui
 {
     // Some example of date formats: "%d/%m/%Y" "%A %d %b %Y"
-    inline IMGUI_API bool DateChooser(const char* label, tm& dateOut, const char* dateFormat = "%d/%m/%Y",
+    IMGUI_API bool DateChooser(const char* label, tm& dateOut, const char* dateFormat = "%d/%m/%Y",
                                 bool* pSetStartDateToDateOutThisFrame = NULL, const char* leftArrow = "<",
                                 const char* rightArrow = ">", const char* upArrowString = "   ^   ",
                                 const char* downArrowString = "   v   ");
 
     // Some helper methods (note that I cannot use tm directly in this header file, so we can't initialize a static
     // date directly with these methods)
-    inline IMGUI_API void SetDateZero(tm* date);
-    inline IMGUI_API void SetDateToday(tm* date);
+    IMGUI_API void SetDateZero(tm* date);
+    IMGUI_API void SetDateToday(tm* date);
 
-    inline IMGUI_API void TestDateChooser(const char* dateFormat = "%d/%m/%Y", const char* leftArrow = "<",
+    IMGUI_API void TestDateChooser(const char* dateFormat = "%d/%m/%Y", const char* leftArrow = "<",
                                     const char* rightArrow = ">", const char* upArrowString = "   ^   ",
                                     const char* downArrowString = "   v   ");
 }
